74b6b18f4c6a6042366c47f3921dde52bc4df91c
Sebastian Keller <sebastian.f.keller@gmail.com>
2025-03-22 14:51:52 +0100

    Optimize IC generation
    early_sync on GPU
    protect makeGlobalBox against empty local domain
    protect nodeCountsKernel against empty grid launches
    added DeviceVector:shrink_to_fit

--- include/cstone/cuda/device_vector.cu
+++ include/cstone/cuda/device_vector.cu
@@ -38,6 +38,7 @@ public:
     void resize(std::size_t size) { data_.resize(size); }
 
     void reserve(std::size_t size) { data_.reserve(size); };
+    void shrink_to_fit() { data_.shrink_to_fit(); };
 
     std::size_t size() const { return data_.size(); };
     std::size_t capacity() const { return data_.capacity(); };
@@ -104,43 +105,49 @@ template<class T>
 T* DeviceVector<T>::data()
 {
     return impl_->data();
-};
+}
 
 template<class T>
 const T* DeviceVector<T>::data() const
 {
     return impl_->data();
-};
+}
 
 template<class T>
 void DeviceVector<T>::resize(std::size_t size)
 {
     impl_->resize(size);
-};
+}
 
 template<class T>
 void DeviceVector<T>::reserve(std::size_t size)
 {
     impl_->reserve(size);
-};
+}
+
+template<class T>
+void DeviceVector<T>::shrink_to_fit()
+{
+    impl_->shrink_to_fit();
+}
 
 template<class T>
 std::size_t DeviceVector<T>::size() const
 {
     return impl_->size();
-};
+}
 
 template<class T>
 bool DeviceVector<T>::empty() const
 {
     return impl_->size() == 0;
-};
+}
 
 template<class T>
 std::size_t DeviceVector<T>::capacity() const
 {
     return impl_->capacity();
-};
+}
 
 template<class T>
 DeviceVector<T>& DeviceVector<T>::swap(DeviceVector<T>& rhs)
