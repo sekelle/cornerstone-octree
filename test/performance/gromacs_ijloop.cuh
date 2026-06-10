798c27ee6dbda584225457069d373e622344b494
Felix Thaler <thaler@cscs.ch>
2026-06-10 05:43:14 +0000

    Fix compilation with CUDA 13.3 (#603)

--- test/performance/gromacs_ijloop.cuh
+++ test/performance/gromacs_ijloop.cuh
@@ -418,9 +418,8 @@ struct GromacsLikeNeighborhood
     Statistics stats() const
     {
         return {.numBodies = lastBody - firstBody,
-                .numBytes  = sciSorted.size() * sizeof(typename decltype(sciSorted)::value_type) +
-                            cjPacked.size() * sizeof(typename decltype(cjPacked)::value_type) +
-                            excl.size() * sizeof(typename decltype(excl)::value_type)};
+                .numBytes =
+                    sciSorted.size() * sizeof(Sci) + cjPacked.size() * sizeof(CjPacked) + excl.size() * sizeof(Excl)};
     }
 };
 
